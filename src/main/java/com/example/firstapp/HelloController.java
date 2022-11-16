a08b60d27f751cae7a040039d832a155c58c7907
s.djuraev <s.djuraev@newuzbekistanuniversity.uz>
2022-11-16 10:37:42 +0500

    V1

--- src/main/java/com/example/firstapp/HelloController.java
+++ src/main/java/com/example/firstapp/HelloController.java
@@ -1,15 +1,43 @@
 package com.example.firstapp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class HelloController {
     @FXML
+    Button LoginButton;
+    @FXML
     private Label welcomeText;
+    @FXML
+    private TextField loginText;
+    @FXML
+    private PasswordField passwordText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    @FXML
+    protected void setLoginButton() throws IOException {
+        File file=new File("password.txt");
+        Scanner scanner=new Scanner(file);
+        while (scanner.hasNext()){
+            String temp[]=scanner.nextLine().split(" ");
+
+            if (loginText.getText().equals((temp[0]))&&passwordText.getText().equals(temp[1]))
+            {
+                System.out.println("Login successful!");
+            }
+        }
+    }
+
+
 }
 // https://github.com/sdjuraev/CS211WEEK71.git
